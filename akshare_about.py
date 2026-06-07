a9173de9d664a32d140ca70b56b8cd02f5815ecc
home_pc <home_pc@fuck.com>
2026-06-07 21:28:32 +0800

    tianjia

--- akshare_about.py
+++ akshare_about.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import mplfinance as mpf
 from datetime import datetime
 
+# 获取数据
 stock_df = ak.stock_zh_a_hist(
     symbol='000001',
     period='daily',
@@ -12,16 +13,42 @@ stock_df = ak.stock_zh_a_hist(
     end_date='20231231'
 )
 
+# 转为英文将列名
+stock_df.rename(columns={
+    '日期': 'date',
+    '开盘': 'open',
+    '收盘': 'close',
+    '最高': 'high',
+    '最低': 'low',
+    '成交量': 'volume',
+    '涨跌幅': 'change_pct'
+}, inplace=True)
+
+#将日期列转换为datetime格式，设置索引为日期
 stock_df['date'] = pd.to_datetime(stock_df['date'])
 stock_df.set_index('date', inplace=True)
 
-plt.plot(stock_df['date'], stock_df['close'])
-sns.histplot(stock_df['return'], bins=50)
-sns.boxplot(stock_df['return'])
+#计算收益率
+stock_df['return'] = stock_df['close'].pct_change() * 100
 
-stock_df_2023 = stock_df['2023-01':'2023-12']
-print(stock_df_2023.head())
+#拆线图
+plt.figure(figsize=(12,4))
+plt.plot(stock_df.index, stock_df['close'])
+plt.title('close price trend')
+plt.show()
 
-print(stock_df_2023.isnull().sum())
+plt.figure(figsize=(10,4))
+sns.histplot(stock_df['return'].dropna(), bins=50)
+plt.title('return distribution')
+plt.show()
 
-print(stock_df)
+plt.figure(figsize=(6,4))
+sns.boxplot(data=stock_df, y='return')
+plt.title('return boxplot')
+plt.show()
+
+mpf.plot(stock_df[['open', 'high', 'low', 'close', 'volume']], type='candle', volume=True, title='k-line chart')
+
+print(stock_df.head())
+
+print(stock_df.isnull().sum())
